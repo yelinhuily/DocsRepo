5cccd5e42a8c347ff6e12f400d00d4e59765a784
yelinhuily <ye.linhui@163.com>
2016-11-21 16:26:37 +0800

    DocsRepo_v1.0

---
+++ ServletDemo/src/demo/HelloServlet.java
@@ -0,0 +1,21 @@
+package demo;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Winter Lau
+ */
+public class HelloServlet extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+		res.getWriter().println("Hello World! This is Ye Lin Hui!");
+	}
+
+}
